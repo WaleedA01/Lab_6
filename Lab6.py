7ad766dc3017c28ed5a6141805250cabc4619427
WaleedA01 <139922385+WaleedA01@users.noreply.github.com>
2023-07-19 01:25:28 -0400

    Add files via upload

---
+++ Lab6.py
@@ -0,0 +1,50 @@
+def password_encoder(password):
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+    return encoded_password
+
+def password_decoder(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_password += decoded_digit
+    return decoded_password
+
+def main():
+    encoded_password = None
+
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+    option = input("Please enter an option: ")
+
+    while option != "3":
+        if option == "1":
+            password = input("Please enter your password to encode: ")
+            encoded_password = password_encoder(password)
+            print("Your password has been encoded and stored!")
+        elif option == "2":
+            if encoded_password is not None:
+                print("The encoded password is", encoded_password, "and the original password is", password_decoder(encoded_password))
+            else:
+                print("No password has been encoded yet!")
+        else:
+            print("Invalid option!")
+
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        option = input("Please enter an option: ")
+
+    print("Exiting the program...")
+
+
+if __name__ == '__main__':
+    main()
